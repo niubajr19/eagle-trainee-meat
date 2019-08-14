c6ab9143706a0d7a21ae3693987f313418d6233f
Lucas Antonio da Silva <lucas.antonio199@outlook.com>
2019-08-14 04:52:01 -0300

    Termino da 2ª Sprint - Colocar itens no carrinho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import{ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service'
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,14 +28,15 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
     RestaurantDetailComponent,
     MenuComponent,
     ShoppingCartComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import{Injectable} from '@angular/core';
 import{Http}from '@angular/http';
 import{MEAT_API} from "../app.api";
 import { Observable } from "rxjs/Observable";
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 @Injectable()
 export class RestaurantsService{
     
@@ -20,5 +21,16 @@ export class RestaurantsService{
        return this.http.get(`${MEAT_API}/restaurants/${id}`)
        .map(response => response.json())
        .catch(ErrorHandler.handleError)
+    }
+    reviewsOfRestaurants(id:string):Observable <any>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+
+    }
+    menuOfRestaurant(id:String):Observable<MenuItem[]>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
     }
   }
